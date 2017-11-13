0cd3991b660f75eb911c6b209e6c81c92435a638
fiona127 <22022783+fiona127@users.noreply.github.com>
2017-11-13 17:40:48 -0500

    with Components

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './Header';
+
+
+
+class App extends React.Component {
+    state = { 
+        pageHeader: 'Naming Contests'
+    };
+    componentDidMount() {
+       
+    }
+    componentWillUnmount() {
+        
+    }
+    render() {
+        return (
+            <div className="App">
+                <Header message={this.state.pageHeader}/>
+                <div>
+                    ...
+                </div>
+            </div>
+        );
+    }
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-const App = (props) => {
-    return (
-        <h2 className="text-center">
-            {props.headerMessage}
-        </h2>
-    );
-};
-
-App.propTypes = {
-    headerMessage: PropTypes.string
-}
-
-App.defaultProps = {
-    headerMessage: 'Hello'
-};
+import App from './components/App';
 
 ReactDOM.render(
     <App  />,
     document.getElementById('root')
 );
+
