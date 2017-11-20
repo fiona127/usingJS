e3bfb96fa01f0e7bbc96732a5bf18c7c6bbb2df1
fiona127 <22022783+fiona127@users.noreply.github.com>
2017-11-20 10:43:40 -0500

    navigation and back

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,10 @@ const pushState = (obj, url) => {
     window.history.pushState(obj, '', url);
 };
 
+const onPopState = handler => {
+    window.onpopstate = handler;
+}
+
 class App extends React.Component {
     static propTypes = {
         initialData: PropTypes.object.isRequired
@@ -20,10 +24,15 @@ class App extends React.Component {
     state = this.props.initialData;
 
     componentDidMount() {
+        onPopState((event) => {
+            this.setState({
+                currentContestId: (event.state ||  {}).currentContestId
+            });
+        });
         
     }
     componentWillUnmount() {
-        //clean timers, listeners
+        onPopState(null);
     }
 
     fetchContest = (contestId) => {
